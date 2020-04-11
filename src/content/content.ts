695c374d852a5279327387f41721daadcc9c1987
Sidharth Malhotra <sidharthmalhotra0094@gmail.com>
2020-04-11 16:58:24 -0400

    [TASK] Added notifier on schedule page and updated manifest

--- src/content/content.ts
+++ src/content/content.ts
@@ -5,7 +5,7 @@ function showNotification(): void {
   Notification.requestPermission();
  else {
   let notification = new Notification("Delivery Slot Available", {
-   icon: "./../assets/img/icon.png",
+   icon: "./assets/img/icon.png",
    body: "One or Few Delivery Slot are Available",
   });
  }
@@ -34,11 +34,35 @@ function initializeNotifier(): void {
   }, 10000)
 }
 
+function initializeWidget(): void {
+  let widgetInterval = setInterval(() => {
+    let scheduleTitle = document.querySelector<HTMLElement>(".ufss-widget-title");
+    let slotNotifier = document.querySelector<HTMLElement>(".slot-notifier-widget");
+    if (!slotNotifier && scheduleTitle && scheduleTitle.innerText === "Schedule your order") {
+      console.log("HIEE");
+      let widget = document.createElement("div");
+      widget.innerHTML = `
+        <div class="slot-notifier-widget" style="background: #8cc63f; display: block; min-height: 50px; width: 300px;
+        z-index: 99999; position: absolute; right: 0; top: 10%; box-shadow: 1px 1px 32px;padding: 12px 10px; border-radius: 5px;">
+          <p style="text-align:center;"><img src="https://i.ibb.co/wrjsMv9/icon.png" alt="icon" border="0" height="50"></p>
+          <h3>
+            Please leave the tab open and I will notify you when a delivery slot is available.
+          </h3>
+        </div>
+      `;
+
+      document.body.appendChild(widget);
+      clearInterval(widgetInterval);
+    }
+  }, 500);
+}
+
 function initializeScript(): void {
   if (interval)
     clearInterval(interval);
   initializeNotifications();
   initializeNotifier();
+  initializeWidget()
 }
 
 // start plugin
