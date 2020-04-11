f2b503a2553a9875137615d19191db2f9be50e72
Sidharth Malhotra <sidharthmalhotra0094@gmail.com>
2020-04-11 12:42:57 -0400

    [TASK] Initialized Extension Structure

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    content: './src/content/content',
+  },
+
+  output: {
+    path: path.resolve(__dirname, './../dist/src/scripts'),
+    filename: '[name].js'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
