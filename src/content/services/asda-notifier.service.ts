5e19cfe47c17106ffb84f71cc2555a0184a9e17d
Sidharth Malhotra <sidharthmalhotra0094@gmail.com>
2020-04-24 13:06:03 -0400

    [FIX] Ada doesn't remove elements from the screen resulting in all the past checked buttons. Added support to add a check class to already checked buttons

--- src/content/services/asda-notifier.service.ts
+++ src/content/services/asda-notifier.service.ts
@@ -7,14 +7,20 @@ export class AsdaNotifier extends Notifier {
   protected initializeNotifier(): void {
     let lastNotificationDelivered = 0;
     let interval = setInterval(() => {
+      let unavailableSlots = 0;
+      let totalSlots = 0;
       let buttonTexts = document.querySelectorAll<HTMLElement>(
         ".co-slots__price-content.co-slots__price-content--box.co-slots__price-content--pointer"
       );
-      let count = 0;
       let notificationFired = false;
       buttonTexts.forEach((btnText) => {
-          if (btnText.textContent && btnText.textContent === "Sold OutC&C?") {
-            count++;
+          totalSlots++;
+          if (!btnText.classList.contains(".delivery-slot-notify-checked") &&
+            btnText.textContent &&
+            btnText.textContent === "Sold OutC&C?"
+          ) {
+            btnText.classList.add("delivery-slot-notify-checked");
+            unavailableSlots++;
           }
           else {
             if (lastNotificationDelivered % 3 === 0 && // tslint:disable-line
@@ -25,7 +31,7 @@ export class AsdaNotifier extends Notifier {
             }
           }
       });
-      if (buttonTexts.length > 0 && count === buttonTexts.length) {
+      if (totalSlots > 0 && totalSlots === unavailableSlots) {
         let nextBtn = document.querySelector<HTMLButtonElement>(
           "[aria-label='Next slots']"
         );
@@ -34,10 +40,8 @@ export class AsdaNotifier extends Notifier {
         }
         else {
           nextBtn.click();
-          notificationFired = false;
           lastNotificationDelivered = -1;
         }
-        count = 0;
       }
       lastNotificationDelivered++;
     }, 3000); // tslint:disable-line
